40ccbeeb09ccc6471adcf2232d4971307787a3de
dkuida <dan@kuida.org>
2018-11-02 03:06:48 +0100

    base ver

---
+++ config/logger.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    service: 'posts',
+    local: true,
+    console: {
+        level: 'info'
+    }
+};
+
--- services/posts.service.js
+++ services/posts.service.js
@@ -1,5 +1,8 @@
 'use strict';
 const MAX_POSTS_COUNT = 3;
+const MAX_EXECUTION_COUNT = process.env.MAX_EXECUTION_COUNT || 2;
+const DIE_AFTER_CALLS = Math.floor(Math.random() * MAX_EXECUTION_COUNT) + 1;
+let overallCalls = 0;
 module.exports = {
 	name: 'posts',
 
@@ -13,6 +16,7 @@ module.exports = {
 				userId: 'number'
 			},
 			handler (ctx) {
+				this.checkIfShouldKill(ctx);
 				const userId = ctx.params.userId;
 				const numberOfPosts = Math.floor(Math.random() * MAX_POSTS_COUNT);
 				const posts = [...new Array(numberOfPosts).keys()];
@@ -25,7 +29,15 @@ module.exports = {
 	events: {},
 
 
-	methods: {},
+	methods: {
+		checkIfShouldKill (ctx) {
+			overallCalls++;
+			if (DIE_AFTER_CALLS > 0 && overallCalls >= DIE_AFTER_CALLS) {
+				this.logger.error(`killing service ${this.fullName} worker ${process.pid}`);
+				process.exit(-1)
+			}
+		}
+	},
 
 	created () {
 
